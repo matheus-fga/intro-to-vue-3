09379cb3dab7aeb46d58c772adf761109eaaf2c7
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-22 14:49:12 -0300

    Updating image and inventory according to the selected variant

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const app = Vue.createApp({
         name: 'Socks',
         description: 'Soft cotton socks',
       },
-      image: './assets/images/socks_green.jpg',
-      inventory: 15,
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
       ]
     }
   },
@@ -19,17 +19,28 @@ const app = Vue.createApp({
     addToCart() {
       if (this.inventory >= 1) {
         this.cart += 1;
-        this.inventory -= 1;
+        this.variants[this.selectedVariant].quantity -= 1;
       }
     },
     removeFromCart() {
       if (this.cart >= 1) {
         this.cart -= 1;
-        this.inventory += 1;
+        this.variants[this.selectedVariant].quantity += 1;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
     },
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product.name}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].quantity;
+    }
   }
 })
