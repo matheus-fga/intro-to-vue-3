79c8fa73dfd5af87b544e73c3de6fbf25f36c311
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-22 14:11:26 -0300

    Disabling 'Add to Cart' button when the product is out of stock

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
         description: 'Soft cotton socks',
       },
       image: './assets/images/socks_green.jpg',
-      inventory: 100,
+      inventory: 15,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -17,15 +17,19 @@ const app = Vue.createApp({
   },
   methods: {
     addToCart() {
-      this.cart += 1;
+      if (this.inventory >= 1) {
+        this.cart += 1;
+        this.inventory -= 1;
+      }
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+        this.inventory += 1;
+      }
     },
     updateImage(variantImage) {
       this.image = variantImage;
     },
-    removeFromCart() {
-      if(this.cart >= 1) {
-        this.cart -= 1;
-      }
-    }
   }
 })
