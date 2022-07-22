732233f1c91eac1c6fac04ab792e8fe134da578e
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-22 15:55:52 -0300

    Creating Vue app components to product display and details

---
+++ components/ProductDisplay.js
@@ -0,0 +1,101 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image" :class="{'out-of-stock-img': inventory < 1}">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>Inventory: {{ inventory }}</p>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+        <p v-else>Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+        <p>{{ product.description }}</p>
+        <product-details :details="details"></product-details>
+        <div 
+          class="color-circle"
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)"
+          :style="{ backgroundColor: variant.color }"
+        >
+        </div>
+        <button 
+          class="button"
+          :class="{ disabledButton: inventory < 1 }" 
+          :disabled="inventory < 1"
+          @click="addToCart" 
+        >
+          Add to Cart
+        </button>
+        <button 
+          class="button"
+          :class="{ disabledButton: cart === 0}" 
+          :disabled="cart === 0"
+          @click="removeFromCart"
+        >
+          Remove Cart
+        </button>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+      product: {
+        name: 'Socks',
+        description: 'Soft cotton socks',
+      },
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      if (this.inventory >= 1) {
+        this.cart += 1;
+        this.variants[this.selectedVariant].quantity -= 1;
+      }
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+        this.variants[this.selectedVariant].quantity += 1;
+      }
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product.name}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free';
+      }
+      
+      return 2.99;
+    }
+  }
+});
--- main.js
+++ main.js
@@ -2,45 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: {
-        name: 'Socks',
-        description: 'Soft cotton socks',
-      },
-      brand: 'Vue Mastery',
-      selectedVariant: 0,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-      ]
+      premium: true,
     }
   },
-  methods: {
-    addToCart() {
-      if (this.inventory >= 1) {
-        this.cart += 1;
-        this.variants[this.selectedVariant].quantity -= 1;
-      }
-    },
-    removeFromCart() {
-      if (this.cart >= 1) {
-        this.cart -= 1;
-        this.variants[this.selectedVariant].quantity += 1;
-      }
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return `${this.brand} ${this.product.name}`;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inventory() {
-      return this.variants[this.selectedVariant].quantity;
-    }
-  }
+  methods: {}
 })
